09403debd0f74dafdd278757836389b4cd4878bb
meprotege <meprotegeoficial@gmail.com>
2025-07-21 14:10:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-console.log("==== TESTE BASICO INICIOU ====");
-setInterval(() => {
-  console.log("Still alive...");
-}, 5000);
+const http = require('http');
+console.log("==== TESTE COM HTTP SERVER INICIOU ====");
+
+const server = http.createServer((req, res) => {
+  res.end("Bot Diana ativo!");
+});
+
+server.listen(21465, () => {
+  console.log("Servidor HTTP ouvindo na porta 21465");
+});
